80055fa3b8ae129bc4b60efd0aef7e1709b207c4
Ager <aghazofficial@gmail.com>
2025-11-28 10:22:50 +0800

    updated

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+	public Slider VolumeSlider;
+	void Start()
+	{
+		// Volume 
+		if (PlayerPrefs.HasKey("soundVolume"))
+		{
+			LoadVolume();
+		}
+		else
+		{
+			PlayerPrefs.SetFloat("soundVolume", 1);
+			LoadVolume();
+		}
+	}
+	//Base Menu Code
+	public void StartGame()
+	{
+
+	}
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+
+	//Volume Code
+	public void SetVolume()
+	{
+		AudioListener.volume = VolumeSlider.value;
+		SaveVolume();
+	}
+	public void SaveVolume()
+	{
+		PlayerPrefs.SetFloat("soundVolume", VolumeSlider.value);
+	}
+	public void LoadVolume()
+	{
+		VolumeSlider.value = PlayerPrefs.GetFloat("soundVolume");
+	}
+
+}
